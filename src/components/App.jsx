b531e4471bbdc899c4f975e38fca51db2060754c
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-10-11 11:23:48 +0300

    4 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import userInfo from '../user.json';
 import { Fragment } from 'react';
 import { FriendList } from './FriendList';
 import { Statistics } from './Statistics';
+import { Transactions } from './Transactions';
 export const App = () => {
   return (
     <Fragment>
       <Profile userInfo={userInfo} />
       <Statistics />
       <FriendList />
+      <Transactions />
     </Fragment>
   );
 };
